2ba1d69d0ddb40633fa497b152253acc9a15b9d9
pirhoo <hello@pirhoo.com>
2017-12-14 16:13:07 +0100

    Fixed parenthesis

--- day1/homework/robot_writer.py
+++ day1/homework/robot_writer.py
@@ -10,7 +10,7 @@ total_unemployment_2014 = 8.0
 
 
 def write_story(municipality, unemployment_2009, unemployment_2014):
-    unemployment_diff = unemployment_2014 - unemployment_2009)
+    unemployment_diff = unemployment_2014 - unemployment_2009
     total_unemployment_diff = total_unemployment_2014 - total_unemployment_2009
     if unemployment_2014 > unemployment_2009:
         print("In 2014, unemployment in %s was %s higher than before the financial crisis in 2009." % (municipality, unemployment_diff))
@@ -19,9 +19,9 @@ def write_story(municipality, unemployment_2009, unemployment_2014):
         print("In 2014, unemployment in %s was %s lower than before the financial crisis in 2009." % (municipality, -unemployment_diff))
         print("It decreased from %s%% to %s%%." % (unemployment_2009, unemployment_2014))
     if unemployment_diff > total_unemployment_diff:
-        print "The development of this city has been a little worse than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff
+        print("The development of this city has been a little worse than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff)
     else:
-        print "The development of this city has been a little better than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff
+        print("The development of this city has been a little better than in the country where the unemployment rate during the same period grew by %s" % total_unemployment_diff)
 
 
 write_story("Stockholm", 7.1, 6.6)
